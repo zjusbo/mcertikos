7f9d998b0d40c4f4ccac5167faf383ab7633d568
Newman Wu <xiongnan.wu@yale.edu>
2015-09-23 16:11:20 -0400

    minor fixes

--- kern/pmm/MContainer/import.h
+++ kern/pmm/MContainer/import.h
@@ -3,10 +3,12 @@
 
 #ifdef _KERN_
 
+unsigned int get_nps(void);
 unsigned int at_is_norm(unsigned int);
 unsigned int at_is_allocated(unsigned int);
 void pmem_init(unsigned int);
 unsigned int palloc(void);
+void pfree(unsigned int);
 
 #endif /* _KERN_ */
 
