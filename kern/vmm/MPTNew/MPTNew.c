8b852e468a1186001e028abd5e4c21229116f946
Haoliang Zhang <hz322@hippo.zoo.cs.yale.edu>
2015-10-13 22:05:45 -0400

    lab2 finished

--- kern/vmm/MPTNew/MPTNew.c
+++ kern/vmm/MPTNew/MPTNew.c
@@ -16,7 +16,12 @@
 unsigned int alloc_page (unsigned int proc_index, unsigned int vaddr, unsigned int perm)
 {
 	// TODO
-	return 0;
+	unsigned int page_index;
+	unsigned int ptbl;
+        page_index = container_alloc(proc_index);
+	if(page_index == 0) return MagicNumber;
+        ptbl = map_page(proc_index, vaddr, page_index, perm);
+        return ptbl;
 }
 
 
--- kern/vmm/MPTOp/MPTOp.c
+++ kern/vmm/MPTOp/MPTOp.c
@@ -1,6 +1,12 @@
 #include <lib/x86.h>
-
 #include "import.h"
+
+#define VA_PDIR_MASK 0xFFC00000
+#define VA_PTBL_MASK 0x3FF000
+#define VM_USERLO  0x40000000
+#define VM_USERHI  0xF0000000
+#define PT_PERM_PWG (PTE_P | PTE_W | PTE_G)
+#define PT_PERM_PW (PTE_P | PTE_W)
 
 /**
  * Returns the page table entry corresponding to the virtual address,
@@ -10,26 +16,53 @@
 unsigned int get_ptbl_entry_by_va(unsigned int proc_index, unsigned int vaddr)
 {
     // TODO
-    return 0;
+    unsigned int pde_index = (vaddr & VA_PDIR_MASK) >> 22;
+    unsigned int pte_index = (vaddr & VA_PTBL_MASK) >> 12;
+
+    unsigned int pde = get_pdir_entry(proc_index, pde_index);
+    // check the present bit of page directory entry
+    if ((pde & PTE_P) == 0) {
+        return 0;
+    }
+    unsigned int pte = get_ptbl_entry(proc_index, pde_index, pte_index);
+    //check the present bit of page table entry
+    if ((pte & PTE_P) == 0) {
+        return 0;
+    }
+    return pte;
 }         
 
 // returns the page directory entry corresponding to the given virtual address
 unsigned int get_pdir_entry_by_va(unsigned int proc_index, unsigned int vaddr)
 {
     // TODO
-    return 0;
+    unsigned int pde_index = (vaddr & VA_PDIR_MASK) >> 22;
+    unsigned int pde = get_pdir_entry(proc_index, pde_index);
+    return pde;
 }
 
 // removes the page table entry for the given virtual address
 void rmv_ptbl_entry_by_va(unsigned int proc_index, unsigned int vaddr)
 {
     // TODO
+    unsigned int pde_index = (vaddr & VA_PDIR_MASK) >> 22;
+    unsigned int pte_index = (vaddr & VA_PTBL_MASK) >> 12;
+
+    unsigned int pde = get_pdir_entry(proc_index, pde_index);
+    // check the present bit of page directory entry
+    if ((pde & PTE_P) == 0) {
+    // the page directory entry is not valid for address translation
+        return;
+    }
+    rmv_ptbl_entry(proc_index, pde_index, pte_index);
 }
 
 // removes the page directory entry for the given virtual address
 void rmv_pdir_entry_by_va(unsigned int proc_index, unsigned int vaddr)
 {
     // TODO
+    unsigned int pde_index = (vaddr & VA_PDIR_MASK) >> 22;
+    rmv_pdir_entry(proc_index, pde_index);
 }
 
 // maps the virtual address [vaddr] to the physical page # [page_index] with permission [perm]
@@ -37,12 +70,17 @@ void rmv_pdir_entry_by_va(unsigned int proc_index, unsigned int vaddr)
 void set_ptbl_entry_by_va(unsigned int proc_index, unsigned int vaddr, unsigned int page_index, unsigned int perm)
 {
     // TODO
+    unsigned int pde_index = (vaddr & VA_PDIR_MASK) >> 22;
+    unsigned int pte_index = (vaddr & VA_PTBL_MASK) >> 12;
+    set_ptbl_entry(proc_index, pde_index, pte_index, page_index, perm);
 }
 
 // registers the mapping from [vaddr] to physical page # [page_index] in the page directory
 void set_pdir_entry_by_va(unsigned int proc_index, unsigned int vaddr, unsigned int page_index)
 {
     // TODO
+    unsigned int pde_index = (vaddr & VA_PDIR_MASK) >> 22;
+    set_pdir_entry(proc_index, pde_index, page_index);
 }   
 
 // initializes the identity page table
@@ -55,4 +93,16 @@ void idptbl_init(unsigned int mbi_adr)
     container_init(mbi_adr);
 
     // TODO
+    unsigned int addr;
+    for (addr = 0; addr < 0xFFFFF000; addr += PAGESIZE) {
+        unsigned int pde_index = (addr & VA_PDIR_MASK) >> 22;
+        unsigned int pte_index = (addr & VA_PTBL_MASK) >> 12;
+        if (addr < VM_USERLO || addr >= VM_USERHI) {
+            //kernel pages
+            set_ptbl_entry_identity(pde_index, pte_index, PT_PERM_PWG);
+        }else {
+            //not kernel pages
+            set_ptbl_entry_identity(pde_index, pte_index, PT_PERM_PW);
+        }
+    }
 }
